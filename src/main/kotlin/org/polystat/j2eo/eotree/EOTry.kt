adb853f658fdbc2937f2f0451fbae3d530b83215
Aleksandr Mishin <mishin.aa@phystech.edu>
2022-12-02 23:38:51 +0300

    documentation

--- src/main/kotlin/org/polystat/j2eo/eotree/EOTry.kt
+++ src/main/kotlin/org/polystat/j2eo/eotree/EOTry.kt
@@ -1,5 +1,12 @@
 package org.polystat.j2eo.eotree
 
+/**
+ * EBNF representation:
+ * `
+ * { try EOL } EOL
+` *
+ */
+
 class EOTry(var bnds: List<EOBnd>) : EONode() {
     var tryline = "try > @\n\t[]\n" // what should I do with indents???
     override fun generateEO(indent: Int): String =
